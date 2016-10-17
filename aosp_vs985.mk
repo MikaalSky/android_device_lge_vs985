27d68b74a17a7a35af8d357816a6545756938b14
MikaalSky <lokio27productions@gmail.com>
2016-10-17 16:52:10 -0500

    Update and rename full_vs985.mk to aosp_vs985.mk

--- aosp_vs985.mk
+++ aosp_vs985.mk
@@ -22,7 +22,12 @@ $(call inherit-product, device/lge/vs985/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := vs985
-PRODUCT_NAME := full_vs985
+PRODUCT_NAME := aosp_vs985
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-VS985
 PRODUCT_MANUFACTURER := LGE
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g3" \
+    PRODUCT_NAME="g3_vzw" \
+    BUILD_FINGERPRINT="lge/g3_vzw/g3:6.0/MRA58K/160141503d43c:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_vzw-user 6.0 MRA58K 160141503d43c release-keys"
